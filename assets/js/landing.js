3af2d907072bd4cc78d17ccb7689a447bb6d8f13
pamarthisai <106539073+pamarthisai@users.noreply.github.com>
2024-08-03 15:47:30 +0530

    Delete assets/js/landing.js

--- assets/js/landing.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const videoButton = document.getElementById('video-btn');
-    
-    if (videoButton) {
-        videoButton.addEventListener('click', () => {
-            window.location.href = '/video';
-        });
-    }
-});
