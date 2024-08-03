c336a8e4e9ab47042ae039b96cb0499fe23d2d51
pamarthisai <106539073+pamarthisai@users.noreply.github.com>
2024-08-03 15:47:51 +0530

    Delete assets/js/index.js

--- assets/js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const landingButton = document.getElementById('landing-btn');
-    
-    if (landingButton) {
-        landingButton.addEventListener('click', () => {
-            window.location.href = '/';
-        });
-    }
-});
